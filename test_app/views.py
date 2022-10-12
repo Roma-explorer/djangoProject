1486b56f303a9d557e77e4f3ca67f12e0910b384
Roma Chub <romachub1@gmail.com>
2022-10-12 13:45:06 +0400

    fix

--- test_app/views.py
+++ test_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
 def test(request):
-    return HttpResponse('Hello')
+    s = request.POST.get('greeting')
+    return JsonResponse({'returned': s.upper()})
